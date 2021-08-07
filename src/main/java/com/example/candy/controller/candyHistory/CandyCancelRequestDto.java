f8af0915589400cd57adfedb744b37edc71b09c6
LateNightCoding <leehae8@naver.com>
2021-08-07 23:54:13 +0900

    feat: 캔디 배정 취소 dto

---
+++ src/main/java/com/example/candy/controller/candyHistory/CandyCancelRequestDto.java
@@ -0,0 +1,10 @@
+package com.example.candy.controller.candyHistory;
+
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+
+@Getter
+@RequiredArgsConstructor
+public class CandyCancelRequestDto {
+    private final Long challengeId;
+}
