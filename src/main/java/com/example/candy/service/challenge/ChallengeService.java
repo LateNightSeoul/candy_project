9e6f5e5bf57269e59a76ec2aefaad754a2488325
LateNightCoding <leehae8@naver.com>
2021-08-07 23:55:50 +0900

    feat: 챌린지 기록 변경 기능 구현

--- src/main/java/com/example/candy/service/challenge/ChallengeService.java
+++ src/main/java/com/example/candy/service/challenge/ChallengeService.java
@@ -6,12 +6,14 @@ import com.example.candy.domain.user.User;
 import com.example.candy.enums.Category;
 import com.example.candy.error.NotFoundException;
 import com.example.candy.repository.challenge.ChallengeHistoryRepository;
+import com.example.candy.repository.challenge.ChallengeHistoryRepositoryImpl;
 import com.example.candy.repository.challenge.ChallengeRepository;
 import com.example.candy.repository.user.UserRepository;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
+import java.util.NoSuchElementException;
 import java.util.Optional;
 
 @Service
@@ -54,5 +56,43 @@ public class ChallengeService {
         return challenge.getId();
     }
 
+    public void assignCandyInChallengeHistory(Long challengeId, int amount, User user) {
+        Optional<ChallengeHistory> findChallengeHistory = challengeHistoryRepository.findByChallenge_id(challengeId);
+        ChallengeHistory saveChallengeHistory;
+        if (findChallengeHistory.isPresent()) {
+            if (findChallengeHistory.get().isComplete() == true || findChallengeHistory.get().getAssignedCandy() != 0) {
+                throw new IllegalStateException("ChallengeHistory Already Exists");
+            }
+            findChallengeHistory.get().setAssignedCandy(amount);
+            saveChallengeHistory = findChallengeHistory.get();
+        } else {
+            Challenge findChallenge = findById(challengeId)
+                    .orElseThrow(() -> new NoSuchElementException("No Such Challenge"));
+            saveChallengeHistory = new ChallengeHistory(user, findChallenge, amount);
+        }
+        saveChallengeHistory(saveChallengeHistory);
+    }
 
+    public int cancelCandyAndGetCandyAmount(Long userId, Long challengeId) {
+        ChallengeHistory findChallengeHistory = challengeHistoryRepository.findByChallenge_idAndUser_id(challengeId, userId)
+                .orElseThrow(() -> new NoSuchElementException("No Such ChallengeHistory"));
+        if (findChallengeHistory.isComplete() || findChallengeHistory.getAssignedCandy() == 0) {
+            throw new IllegalStateException("Can't Cancel Candy From ChallengeHistory");
+        }
+        int candyAmount = findChallengeHistory.getAssignedCandy();
+        findChallengeHistory.setAssignedCandy(0);
+        return candyAmount;
+    }
+
+    public Optional<Challenge> findById(Long challengeId) {
+        return challengeRepository.findById(challengeId);
+    }
+
+    public ChallengeHistory saveChallengeHistory(ChallengeHistory challengeHistory) {
+        return challengeHistoryRepository.save(challengeHistory);
+    }
+
+    public Challenge saveChallenge(Challenge challenge) {
+        return challengeRepository.save(challenge);
+    }
 }
