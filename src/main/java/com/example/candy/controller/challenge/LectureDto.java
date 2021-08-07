2c938a0236ac860c026d61ddd83c0971272b08fd
wnsgur9701 <62540133+wnsgur9701@users.noreply.github.com>
2021-08-07 22:13:14 +0900

    Create LectureDto.java

---
+++ src/main/java/com/example/candy/controller/challenge/LectureDto.java
@@ -0,0 +1,26 @@
+package com.example.candy.controller.challenge;
+
+import com.example.candy.domain.challenge.Challenge;
+import com.example.candy.domain.lecture.Lecture;
+import lombok.Getter;
+import lombok.Setter;
+
+import javax.persistence.*;
+
+@Getter @Setter
+public class LectureDto {
+
+    private String videoUrl;
+
+    private String content;
+
+    private String fileUrl;
+
+    public Lecture toEntity() {
+        return Lecture.builder()
+                .videoUrl(videoUrl)
+                .content(content)
+                .fileUrl(fileUrl)
+                .build();
+    }
+}
