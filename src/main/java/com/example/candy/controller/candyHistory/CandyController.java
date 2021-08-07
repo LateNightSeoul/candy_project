f64fb6fe586b0c8943d26503649eb8f16f8f8514
LateNightCoding <leehae8@naver.com>
2021-08-07 20:14:30 +0900

    feat: 캔디 조회, 충전, 인출 기능 구현

--- src/main/java/com/example/candy/controller/candyHistory/CandyController.java
+++ src/main/java/com/example/candy/controller/candyHistory/CandyController.java
@@ -1,17 +1,44 @@
 package com.example.candy.controller.candyHistory;
 
 import com.example.candy.controller.ApiResult;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import com.example.candy.security.JwtAuthentication;
+import com.example.candy.service.candyHistory.CandyHistoryService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/candy")
+@RequiredArgsConstructor
 public class CandyController {
 
-//    @PostMapping("/charge")
-//    public ApiResult<CandyResponseDto> chargeCandy(@RequestBody CandyChargeRequestDto candyChargeRequestDto) {
-//        return ApiResult("hi");
-//    }
+    private final CandyHistoryService candyHistoryService;
+
+    @GetMapping("/student")
+    public ApiResult<CandyResponseDto> candyStudent(@AuthenticationPrincipal JwtAuthentication authentication) {
+        System.out.println(authentication);
+        return ApiResult.OK(new CandyResponseDto(candyHistoryService.candyStudent(authentication.id)));
+    }
+
+    @GetMapping("/parent")
+    public ApiResult<CandyResponseDto> candyParent(@AuthenticationPrincipal JwtAuthentication authentication) {
+        return ApiResult.OK(new CandyResponseDto(candyHistoryService.candyParent(authentication.id)));
+    }
+
+    @PostMapping("/charge")
+    public ApiResult<CandyResponseDto> chargeCandy(@AuthenticationPrincipal JwtAuthentication authentication,
+            @RequestBody CandyChargeRequestDto candyChargeRequestDto) {
+        return ApiResult.OK(new CandyResponseDto(
+                candyHistoryService.chargeCandy(authentication.id,
+                candyChargeRequestDto.getAmount()).getParentCandy()));
+    }
+
+    @PostMapping("/withdraw")
+    public ApiResult<CandyWithdrawResponseDto> withdrawCandy(@AuthenticationPrincipal JwtAuthentication authentication,
+                                                             @RequestBody CandyWithdrawRequestDto candyWithdrawRequestDto) {
+        return ApiResult.OK(new CandyWithdrawResponseDto(
+                candyHistoryService.withdrawCandy(authentication.id, candyWithdrawRequestDto.getAmount()).getStudentCandy(),
+                candyWithdrawRequestDto.getAmount())
+        );
+    }
 }
