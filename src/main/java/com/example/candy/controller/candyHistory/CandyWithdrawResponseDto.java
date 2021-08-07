1571272883570f4520ab84961725bd69baa65344
LateNightCoding <leehae8@naver.com>
2021-08-07 20:14:11 +0900

    feat: 캔디 인출 dto 구현

---
+++ src/main/java/com/example/candy/controller/candyHistory/CandyWithdrawResponseDto.java
@@ -0,0 +1,24 @@
+package com.example.candy.controller.candyHistory;
+
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+import org.apache.commons.lang3.builder.ToStringBuilder;
+import org.apache.commons.lang3.builder.ToStringStyle;
+
+@Getter
+@RequiredArgsConstructor
+public class CandyWithdrawResponseDto {
+
+    private final int withdrewCandy;
+    private final int candyAmount;
+
+    @Override
+    public String toString() {
+        return new ToStringBuilder(this, ToStringStyle.SHORT_PREFIX_STYLE)
+                .append("withdrewCandy", withdrewCandy)
+                .append("candyAmount", candyAmount)
+                .toString();
+    }
+
+
+}
