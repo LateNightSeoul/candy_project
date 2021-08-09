d36c18a866a5d18f6c995d977dd8a61c89cc669d
wnsgur9701 <62540133+wnsgur9701@users.noreply.github.com>
2021-08-09 19:22:43 +0900

    Update ChallengeRepository.java

--- src/main/java/com/example/candy/repository/challenge/ChallengeRepository.java
+++ src/main/java/com/example/candy/repository/challenge/ChallengeRepository.java
@@ -1,7 +1,6 @@
 package com.example.candy.repository.challenge;
 
 import com.example.candy.domain.challenge.Challenge;
-import com.example.candy.domain.user.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
