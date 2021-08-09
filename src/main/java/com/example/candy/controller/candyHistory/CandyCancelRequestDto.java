156f05ef17396780ba04c50c1543e174caf9ecaf
LateNightCoding <leehae8@naver.com>
2021-08-09 23:44:04 +0900

    feat: Swagger 구현

--- src/main/java/com/example/candy/controller/candyHistory/CandyCancelRequestDto.java
+++ src/main/java/com/example/candy/controller/candyHistory/CandyCancelRequestDto.java
@@ -1,11 +1,12 @@
 package com.example.candy.controller.candyHistory;
 
+import io.swagger.annotations.ApiModelProperty;
 import lombok.Getter;
 import lombok.RequiredArgsConstructor;
 
 @Getter
-
 public class CandyCancelRequestDto {
+    @ApiModelProperty
     private Long challengeId;
     public CandyCancelRequestDto() {}
     public CandyCancelRequestDto(Long challengeId) {this.challengeId = challengeId;}
--- src/main/java/com/example/candy/controller/candyHistory/CandyChargeRequestDto.java
+++ src/main/java/com/example/candy/controller/candyHistory/CandyChargeRequestDto.java
@@ -1,6 +1,7 @@
 package com.example.candy.controller.candyHistory;
 
 import com.example.candy.domain.candy.EventType;
+import io.swagger.annotations.ApiModelProperty;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.RequiredArgsConstructor;
@@ -8,6 +9,7 @@ import org.springframework.web.bind.annotation.RequestBody;
 
 @Getter
 public class CandyChargeRequestDto {
+    @ApiModelProperty(value = "수행할 캔디 양")
     private int amount;
     public CandyChargeRequestDto(int amount) {this.amount = amount;}
     public CandyChargeRequestDto() {}
