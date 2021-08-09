bcdce5d06e1826ade35fb330072ce5f99a1e40c2
LateNightCoding <leehae8@naver.com>
2021-08-09 23:25:08 +0900

    fix: Swagger 리스트 조회 오류 디버그

--- src/main/java/com/example/candy/configure/SwaggerConfigure.java
+++ src/main/java/com/example/candy/configure/SwaggerConfigure.java
@@ -18,7 +18,7 @@ public class SwaggerConfigure {
         return new Docket(DocumentationType.SWAGGER_2)
                 .apiInfo(apiInfo())
                 .select()
-                .apis(RequestHandlerSelectors.basePackage("com.hobbybee"))
+                .apis(RequestHandlerSelectors.any())
                 .paths(PathSelectors.any())
                 .build();
     }
