2f6ee35152b6fcde270de6bb4efa02d824bd40f6
wnsgur9701 <62540133+wnsgur9701@users.noreply.github.com>
2021-08-09 19:21:30 +0900

    Update ChallengeController.java
    add like // likeList

--- src/main/java/com/example/candy/controller/challenge/ChallengeController.java
+++ src/main/java/com/example/candy/controller/challenge/ChallengeController.java
@@ -2,18 +2,19 @@ package com.example.candy.controller.challenge;
 
 import com.example.candy.controller.ApiResult;
 import com.example.candy.domain.challenge.Challenge;
+import com.example.candy.domain.challenge.ChallengeLike;
 import com.example.candy.domain.choice.Choice;
 import com.example.candy.domain.lecture.Lecture;
 import com.example.candy.domain.problem.Problem;
+import com.example.candy.security.JwtAuthentication;
+import com.example.candy.service.challenge.ChallengeLikeService;
 import com.example.candy.service.challenge.ChallengeService;
 import com.example.candy.service.choice.ChoiceService;
 import com.example.candy.service.lecture.LectureService;
 import com.example.candy.service.problem.ProblemService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.web.bind.annotation.*;
 
 import java.time.LocalDateTime;
 import java.util.ArrayList;
@@ -24,6 +25,8 @@ public class ChallengeController {
 
     @Autowired
     private ChallengeService challengeService;
+    @Autowired
+    private ChallengeLikeService challengeLikeService;
 
     @PostMapping("/register")
     public ApiResult<ChallengeRegisterResponseDto> register(@RequestBody ChallengeRegisterRequestDto challengeRegisterRequestDto) {
@@ -80,9 +83,46 @@ public class ChallengeController {
         Challenge findChallenge = challengeService.registerChallenge(challenge);
 
         return ApiResult.OK(new ChallengeRegisterResponseDto(findChallenge));
+    }
 
 
+    /*
+    @GetMapping("/all")
+    public ApiResult<List<ChallengeDto>> challenges(@AuthenticationPrincipal JwtAuthentication authentication) {
+        List<ChallengeDto> challengeDtoList = new ArrayList<>();
+        List<Challenge> challengeList = challengeService.findAllChallenges();
+        List<ChallengeLike> challengeLikes = challengeLikeService.findChallengeLikes(authentication.id);
+        for (ChallengeLike challengeLike : challengeLikes) {
+            challengeLike.getChallenge().getId();
+
+        }
+
+    }
+
+     */
 
 
+    @PostMapping("{challengeId}/like")
+    public ApiResult<Long> like(
+            @AuthenticationPrincipal JwtAuthentication authentication,
+            @PathVariable Long challengeId
+    ) {
+        ChallengeLike saved = challengeLikeService.like(authentication.id, challengeId);
+        return ApiResult.OK(saved.getId());
+    }
+
+    @GetMapping("likeList")
+    public ApiResult<List<ChallengeDto>> likeList(
+            @AuthenticationPrincipal JwtAuthentication authentication
+    ) {
+        List<ChallengeDto> challengeDtoList = new ArrayList<>();
+        List<ChallengeLike> challengeLikeList = challengeLikeService.findAll(authentication.id);
+        for (ChallengeLike challengeLike : challengeLikeList) {
+            Challenge challenge = challengeLike.getChallenge();
+            ChallengeDto challengeDto = new ChallengeDto(challenge.getId(), challenge.getCategory(), challenge.getTitle(),
+                    challenge.getSubTitle(),true,challenge.getTotalScore(),challenge.getRequiredScore());
+            challengeDtoList.add(challengeDto);
+        }
+        return ApiResult.OK(challengeDtoList);
     }
 }
