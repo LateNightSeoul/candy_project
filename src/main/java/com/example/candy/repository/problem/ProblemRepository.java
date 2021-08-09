2039f05b1bf55288828d03ed3faa49ecf38872f5
wnsgur9701 <62540133+wnsgur9701@users.noreply.github.com>
2021-08-10 00:48:14 +0900

    Update ProblemRepository.java
    delete import user

--- src/main/java/com/example/candy/repository/problem/ProblemRepository.java
+++ src/main/java/com/example/candy/repository/problem/ProblemRepository.java
@@ -1,7 +1,6 @@
 package com.example.candy.repository.problem;
 
 import com.example.candy.domain.problem.Problem;
-import com.example.candy.domain.user.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
