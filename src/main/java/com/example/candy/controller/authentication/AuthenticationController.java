9ed440d89fc9f6d97c6dce2bd836f93ea08951e1
LateNightCoding <leehae8@naver.com>
2021-08-09 23:25:43 +0900

    feat: Swagger 구현

--- src/main/java/com/example/candy/controller/authentication/AuthenticationController.java
+++ src/main/java/com/example/candy/controller/authentication/AuthenticationController.java
@@ -4,6 +4,9 @@ import com.example.candy.controller.ApiResult;
 import com.example.candy.security.AuthenticationRequest;
 import com.example.candy.security.AuthenticationResult;
 import com.example.candy.security.JwtAuthenticationToken;
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
+import io.swagger.annotations.ApiParam;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.core.Authentication;
@@ -19,13 +22,15 @@ import static com.example.candy.controller.ApiResult.OK;
 
 @RestController
 @RequestMapping("/auth")
+@Api(tags = {"로그인"})
 public class AuthenticationController {
 
     @Autowired
     private AuthenticationManager authenticationManager;
 
     @PostMapping("/authenticate")
-    public ApiResult<AuthenticationResponseDto> authentication(@RequestBody AuthenticationRequest authRequest) {
+    @ApiOperation(value = "로그인")
+    public ApiResult<AuthenticationResponseDto> authentication(@ApiParam @RequestBody AuthenticationRequest authRequest) {
         try {
             JwtAuthenticationToken authToken = new JwtAuthenticationToken(authRequest.getEmail(), authRequest.getPassword());
             Authentication authentication = authenticationManager.authenticate(authToken);
