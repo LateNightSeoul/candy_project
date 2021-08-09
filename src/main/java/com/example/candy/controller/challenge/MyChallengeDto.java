4ad8a27a8687c488b03b2e8aed73a344314ab29b
wnsgur9701 <62540133+wnsgur9701@users.noreply.github.com>
2021-08-10 00:47:27 +0900

    Create MyChallengeDto.java

---
+++ src/main/java/com/example/candy/controller/challenge/MyChallengeDto.java
@@ -0,0 +1,29 @@
+package com.example.candy.controller.challenge;
+
+import com.example.candy.enums.Category;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter @Setter
+public class MyChallengeDto {
+
+    private Long id;
+    private Category category;
+    private String title;
+    private String subTitle;
+    private int totalScore;
+    private int requiredScore;
+    private int assignedCandy;
+    private boolean complete;
+
+    public MyChallengeDto(Long id, Category category, String title, String subTitle, int totalScore, int requiredScore, int assignedCandy ,boolean complete) {
+        this.id = id;
+        this.category = category;
+        this.title = title;
+        this.subTitle = subTitle;
+        this.totalScore = totalScore;
+        this.requiredScore = requiredScore;
+        this.assignedCandy = assignedCandy;
+        this.complete = complete;
+    }
+}
