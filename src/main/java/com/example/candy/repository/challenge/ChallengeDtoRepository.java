880cec6dfeb5ed834f5470ed8c5d14e17fc9fedf
wnsgur9701 <62540133+wnsgur9701@users.noreply.github.com>
2021-08-16 18:10:23 +0900

    Update ChallengeDtoRepository.java
    폴더명 수정

--- src/main/java/com/example/candy/repository/challenge/ChallengeDtoRepository.java
+++ src/main/java/com/example/candy/repository/challenge/ChallengeDtoRepository.java
@@ -20,7 +20,7 @@ public class ChallengeDtoRepository {
     public List<ChallengeDto> findChallenges(Long userId) {
         return em.createQuery(
                 "select new " +
-                        "com.example.candy.controller.challenge.ChallengeDto(c.id,c.category,c.title,c.subTitle," +
+                        "com.example.candy.controller.challenge.dto.ChallengeDto(c.id,c.category,c.title,c.subTitle," +
                         "cl.id," +
                         "c.totalScore,c.requiredScore)" +
                         " from Challenge c" +
@@ -34,7 +34,7 @@ public class ChallengeDtoRepository {
     public Optional<ChallengeDetailResponseDto> findChallengeDetail(Long userId, Long challengeId) {
         ChallengeDetailResponseDto challengeDetailResponseDto = em.createQuery(
                 "select new " +
-                        "com.example.candy.controller.challenge.ChallengeDetailResponseDto(" +
+                        "com.example.candy.controller.challenge.dto.ChallengeDetailResponseDto(" +
                         "c.title, c.subTitle, ch.assignedCandy, cl.id, c.category, c.description," +
                         "c.totalScore, c.requiredScore, c.level, c.problemCount" +
                         ")" +
