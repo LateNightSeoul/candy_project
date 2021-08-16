997dbdaedbab358c5fb8fb86f109da108f5851cf
LateNightCoding <leehae8@naver.com>
2021-08-16 21:54:41 +0900

    feat: swagger api 구현

--- src/main/java/com/example/candy/controller/user/dto/UserInfoResponseDto.java
+++ src/main/java/com/example/candy/controller/user/dto/UserInfoResponseDto.java
@@ -1,12 +1,17 @@
 package com.example.candy.controller.user.dto;
 
+import io.swagger.annotations.ApiModelProperty;
 import lombok.NoArgsConstructor;
 
 @NoArgsConstructor
 public class UserInfoResponseDto {
+    @ApiModelProperty
     private String email;
+    @ApiModelProperty
     private String name;
+    @ApiModelProperty
     private String phone;
+    @ApiModelProperty
     private String birth;
 
     public UserInfoResponseDto(String email, String name, String phone, String birth) {
