6690392fd20665ae85bc0720d7e256a39e161838
LateNightSeoul <60308568+LateNightSeoul@users.noreply.github.com>
2021-08-22 21:07:55 +0900

    feat: swagger 비밀번호 찾기 부가 설명 추가

--- src/main/java/com/example/candy/controller/user/UserController.java
+++ src/main/java/com/example/candy/controller/user/UserController.java
@@ -67,9 +67,9 @@ public class UserController {
     public ApiResult<Boolean> validate(@RequestBody @ApiParam ValidateEmailRequestDto validateEmailRequestDto) throws NotFoundException {
     	return ApiResult.OK(userService.validate(validateEmailRequestDto.getEmail(), validateEmailRequestDto.getAuth()));
     }
-    
+
     @PostMapping("/new_pw")
-    @ApiOperation(value = "새로운 비밀번호 설정", notes = "이메일 인증했을 시 true, 인증하지 않았을 시 false 반환")
+    @ApiOperation(value = "새로운 비밀번호 설정 (비밀번호 찾기 시)", notes = "이메일 인증했을 시 true, 인증하지 않았을 시 false 반환")
     public ApiResult<Boolean> findPassword(@RequestBody @ApiParam NewPasswordRequestDto newPasswordRequestDto) throws NotFoundException {
     	return ApiResult.OK(userService.new_pw(newPasswordRequestDto.getEmail(), newPasswordRequestDto.getPassword()));
     }
@@ -88,6 +88,7 @@ public class UserController {
     }
 
     @PostMapping("/password/change")
+    @ApiOperation(value = "새로운 비밀번호 설정 (로그인 후 유저 정보 페이지에서)", notes = "이메일 인증 필요 X")
     public ApiResult<Boolean> changePassword(@AuthenticationPrincipal JwtAuthentication authentication,
                                              @RequestBody @ApiParam ChangePasswordRequestDto changePasswordRequestDto) throws NotFoundException {
         userService.changePassword(authentication.id, changePasswordRequestDto.getNewPassword());
